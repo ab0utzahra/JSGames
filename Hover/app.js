9884c15ef6012cbe25880e93f48932a9f270fe4a
Zahra Aledavood <zarsdt@gmail.com>
2023-06-30 18:05:25 +0330

    mouse hover

---
+++ Hover/app.js
@@ -0,0 +1,13 @@
+const hover = document.querySelector('.circle');
+
+hover.addEventListener('mouseenter', ()=> {
+    if(!hover.classList.contains('hover')){
+        hover.classList.add('hover');
+    }
+})
+
+hover.addEventListener('mouseleave', ()=> {
+    if(hover.classList.contains('hover')){
+        hover.classList.remove('hover');
+    }
+})
