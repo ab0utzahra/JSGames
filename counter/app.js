46816419511c107a384250b302e4e8aaf3f096a4
Zahra Aledavood <zarsdt@gmail.com>
2023-07-05 09:51:14 +0330

    Counter

---
+++ counter/app.js
@@ -0,0 +1,14 @@
+// SELECTORES
+const resetbtn = document.querySelector('reset');
+const desbtn = document.querySelector('descrease');
+const inbtn = document.querySelector('increase');
+const container = document.getElementById('#contain');
+
+
+// EVENT LISTENERS
+
+
+
+
+
+// FUNCTIONS
