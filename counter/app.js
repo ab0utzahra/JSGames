796d55eb8d406258b6b5f10c4ae3ead580dba5d1
Zahra Aledavood <zarsdt@gmail.com>
2023-07-06 19:09:23 +0330

    counter finish

--- counter/app.js
+++ counter/app.js
@@ -1,14 +1,30 @@
 // SELECTORES
-const resetbtn = document.querySelector('reset');
-const desbtn = document.querySelector('descrease');
-const inbtn = document.querySelector('increase');
-const container = document.getElementById('#contain');
-
+const resetbtn = document.querySelector('.reset');
+const desbtn = document.querySelector('.descrease');
+const inbtn = document.querySelector('.increase');
+const container = document.getElementById('contain');
+const btns = document.querySelectorAll('.btn');
+let count = 0;
 
 // EVENT LISTENERS
 
+desbtn.addEventListener("click", ()=>{
+   count++ ;
+   container.textContent = count;
+});
 
-
-
+btns.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        const style = e.currentTarget.classList;
+        if (style.contains("increase")){
+            count -- ;
+            
+        }
+        if (style.contains("reset")) {
+            count = 0 ;
+        }
+        container.textContent = count;
+    })
+})
 
 // FUNCTIONS
